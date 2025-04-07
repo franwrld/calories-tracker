f7682234f783a6b29661abd5ae728670781ec88f
franwrld <franwrld22@gmail.com>
2025-04-07 11:09:34 -0600

    Primer Commit CaloriesTracker

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import Form from "./components/Form"
+
+function App() {
+
+  return (
+    <>
+      <header className="bg-indigo-400 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">
+            Contador de Calorias
+          </h1>
+        </div>
+      </header>
+
+      <section className="bg-indigo-200 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <form action="">
+            <Form 
+            
+            />
+          </form>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Form.tsx
@@ -0,0 +1,54 @@
+import { categories } from "../data/categories"
+
+export default function Form() {
+  return (
+    <form 
+        action=""
+        className="space-y-5 bg-white shadow p-10 rounded-lg"
+    >
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="category" className="font-bold">Categoria:</label>
+            <select 
+                className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+                id="category"
+            >
+                {categories.map(category => (
+                    <option 
+                        key={category.id}
+                        value={category.id}
+                    >
+                        {category.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="activity" className="font-bold">Actividad:</label>
+            <input 
+                type="text" 
+                id="activity"
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Ej. Comida, Jugo, Ensalada.."
+            />
+        </div>
+
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="calories" className="font-bold">Calorias:</label>
+            <input 
+                type="number" 
+                id="calories"
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Calorias. 400 o 500.."
+            />
+        </div>
+
+        <input 
+            type="submit" 
+            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+            value="Guardar"
+        />
+
+    </form>
+  )
+}
