8f180856358119b9d1b2ef393402fbfa7520b07d
franwrld <franwrld22@gmail.com>
2025-04-07 15:35:18 -0600

    Accion de eliminar hecho

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import { useMemo, Dispatch } from "react"
 import { Activity} from "../types"
 import { categories } from "../data/categories"
-import { PencilSquareIcon } from '@heroicons/react/24/outline'
+import { PencilSquareIcon, XCircleIcon } from '@heroicons/react/24/outline'
 import { ActivityActions } from "../reducers/activityReducer"
 
 type ActivityListProps = {
@@ -40,6 +40,14 @@ export default function ActivityList({activities, dispatch} : ActivityListProps)
                         >
                             <PencilSquareIcon 
                                 className="h-8 w-8 text-gray-800 cursor-pointer"
+                            />
+                        </button>
+
+                        <button
+                            onClick={() => dispatch({type: "delete-activity", payload: {id: activity.id}})}
+                        >
+                            <XCircleIcon 
+                                className="h-8 w-8 text-red-500 cursor-pointer"
                             />
                         </button>
                     </div>
